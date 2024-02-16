b0f85c476f488f150ff7fca223473c88e69149d5
Arnis Gustins <arnis.gustins@testdevlab.com>
2024-02-16 19:14:59 +0200

    add spaces

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,5 +1,5 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import { browser,$} from "@wdio/globals";
+import { browser, $ } from "@wdio/globals";
 
 Given("Iam on login page", async function() {
     await browser.navigateTo("https://the-internet.herokuapp.com/login");
